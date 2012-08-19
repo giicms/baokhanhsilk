593dcf52c3d30d34800ea9babefbf6b93e52a76f
ukyotachibana <dtqsdn@gmail.com>
2012-08-19 23:45:41 +0700

    add, edit, delete on collection table

---
+++ app/Model/Collection.php
@@ -0,0 +1,16 @@
+<?php
+App::uses('AuthComponent', 'Controller/Component');
+class Collection extends AppModel {
+	public $name = 'Collection';
+	
+	var $validate = array(
+		/*'name' => array(
+			'required'   => true,
+			'allowEmpty' => false,
+			'on'         => 'create',
+			'message' => 'This field is required.'
+		)*/
+	);
+}
+
+?>
